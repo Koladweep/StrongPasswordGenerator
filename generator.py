2f9c8fdaf8f188176402a086249823c4ee1fed25
Nayan Chandwani <102764937+Koladweep@users.noreply.github.com>
2023-07-04 14:41:07 +0530

    simplify and clean generator.py
    removed unused dependencies and added a dataframe for cleaner output

--- generator.py
+++ generator.py
@@ -1,54 +1,80 @@
 from time import sleep
-import secrets
-import string
-import random
-import zxcvbn
-from decimal import*
+from secrets import choice
+from zxcvbn import zxcvbn
+from pandas import DataFrame as DF
+
+password_length = 0
+while not (17 <= password_length <= 25):
+    try:
+        password_length = int(input('Set password length between 17 to 25: '))
+    except:
+        print('Invalid input. Enter a value between 17 and 25.\n\n')
 
 def generate_password(length):
     # Define the character set to be used in the password generation
-    #character_set = string.ascii_letters + string.digits + string.punctuation
     qwerty_characters = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+{}|:\"<>?"
     qwerty_characters = jumble_string(qwerty_characters)
     password = []
     last_char = ''
     while len(password) < length:
         # Choose a random character from the character set
-        char = secrets.choice( qwerty_characters)
+        char = choice(qwerty_characters)
         # Check if the character meets the conditions for inclusion in the password
         if char != last_char and password.count(char) < 2 and char in qwerty_characters:
             password.append(char)
             last_char = char
-            qwerty_characters=jumble_string(qwerty_characters)
+            qwerty_characters = jumble_string(qwerty_characters)
     # Convert the password list to a string, jumble it and return
-    password=jumble_string(password)#... we are skipping this step to avoid undermining the logic that prevents clustering
+    password = jumble_string(password)  # ... we are skipping this step to avoid undermining the logic that prevents clustering
     return ''.join(password)
-# a method that jumnles the provided string( in our case, the qwerty set of keystrokes to provide an added layer of randomness
+
+# a method that jumbles the provided string (in our case, the qwerty set of keystrokes to provide an added layer of randomness)
 def jumble_string(string):
     chars = list(string)
-    jumbled_string=''
-    l=len(chars)
+    jumbled_string = ''
+    l = len(chars)
     for i in range(l):
-        c=secrets.choice(chars)
+        c = choice(chars)
         chars.remove(c)
-        jumbled_string=jumbled_string+c
-    
+        jumbled_string = jumbled_string + c
+
     return jumbled_string
 
 
 passlist = []
 # Generate 20 passwords and add them to the passlist
 for i in range(20):
-    # Choose a random password length between 16 and 20 characters
-    password_length = 20#random.randint(16, 20)
     # Generate a password of the chosen length and add it to the passlist
     passlist.append(generate_password(password_length))
-print('scores on a scale from 0 to 4 where 0 means bad and 4 means very strong and 3 is average')
-#printing scores for each password suggestion along with estimated time to crack using standard brute force tests
-for i in passlist:
-  insights=zxcvbn.zxcvbn(i)
-  print(f"suggestion: {i}, score:{insights['score']}, cracktime offline fasthashing 1e10 per second: {insights['crack_times_display']['offline_fast_hashing_1e10_per_second']}")
-print('press ctrl+c to terminate\n')
+
+print('\nScores on a scale of 0 to 4 and respective cracktime estimates for the suggested passwords\n\n')
+
+# Create an empty list to store the results
+results = []
+
+# Calculate scores for each password suggestion along with estimated time to crack using standard brute force tests
+for i in range(20):
+    insights = zxcvbn(passlist[i])
+
+    results.append({'Sl.No.':i+1,
+        'suggested password': passlist[i],
+        'score': insights['score'],'@100/hr':insights['crack_times_display']['online_throttling_100_per_hour'], '@36,000/hr':insights['crack_times_display']['online_no_throttling_10_per_second'], '@196,000/hr':insights['crack_times_display']['offline_slow_hashing_1e4_per_second'], '@792.9M/hr':insights['crack_times_display']['offline_fast_hashing_1e10_per_second']})
+
+
+# Define a formatter function to center the text and add spaces
+def formatter(x):
+    return f'  {str(x).center(4)}  '
+
+# Create a dataframe from the results
+df=DF(results)
+
+# Apply the formatter to all columns
+formatters = {col: formatter for col in df.columns}
+
+
+# Print the dataframe with centered text and extra spaces between columns
+print(df.to_string(index=False, formatters=formatters))
+print('\nPress ctrl+c if you want to exit.\n')
 for i in range(30):
-	sleep(.999)
-	print(f'\r Autoterminating in {60-i-1} seconds', end='', flush=True)
+    sleep(.999)
+    print(f'\r Autoterminating in {30 - i - 1} seconds', end='', flush=True)
